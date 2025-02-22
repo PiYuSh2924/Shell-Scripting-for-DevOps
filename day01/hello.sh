b07db8612208e19998eebed08366ed60ac229ad9
Ubuntu <ubuntu@ip-172-31-2-102.us-east-2.compute.internal>
2025-02-22 05:07:02 +0000

    Initial commit

---
+++ day01/hello.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#This is an automating script
+
+echo "Hello Guys!"
+
+echo "All these lines are gonna be executed by itself."
+
+echo "#Daily Digest:
+#a normal text file appears white
+#an executeable file appears green
+#a folder appears blue
+#
+#to make a file executeable you need to give it executable permissions
+#write chmod 744 to give the admin(i.e. you) the ability to execute the file
+#first number is for admin
+#second number for the group
+#third number for the rest of the users
+#
+#7 -> read write execute
+#5 -> read execute
+#4 -> read only"
---
+++ day02/shabang.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This is a single line comment
+
+<< comment
+This is how 
+we write a multiline comment.
+Comments are not executed.
+comment
+
+name="Piyush" 
+
+echo "My name is $name"
+
+echo "Enter your name: "
+
+read username
+
+echo "Hello $username ✌️, are you having fun learning shell scripting?"
+
+
